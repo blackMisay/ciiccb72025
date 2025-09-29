87b3456f287431c67aac6c47b08024b66f3a1fce
Russelle Carolino <russelle.carolino@hotmail.com>
2025-09-29 11:09:40 +0800

    CIICC9 - Task16 program

---
+++ chapter6/Task16/Animal.java
@@ -0,0 +1,7 @@
+package ciiccb72025.chapter6.Task16;
+
+public interface Animal {
+    boolean feed(boolean timeToEat);
+    void groom();
+    void pet();
+}
---
+++ chapter6/Task16/Gorilla.java
@@ -0,0 +1,16 @@
+package ciiccb72025.chapter6.Task16;
+
+public class Gorilla implements Animal {
+
+    public boolean feed(boolean timeToEat) {
+        // put gorilla food in the cage
+    }
+
+    public void groom() {
+        // lather, rinse, repeat
+    }
+
+    public void pet() {
+        // pet at your own risk    
+    }
+}
